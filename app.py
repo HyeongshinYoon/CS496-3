75cde3063d3c5abc496e5e58a81c5c3ad7b4418c
HyeongshinYoon <35055625+HyeongshinYoon@users.noreply.github.com>
2019-07-15 20:27:17 +0900

    edit app.py

--- app.py
+++ app.py
@@ -1,232 +1,74 @@
-# import pyrebase
-# from flask import *
-# from oauth2client.contrib.flask_util import UserOAuth2
-#
-# config = {
-#     "apiKey": "AIzaSyDmn0dJl4lvTASIPtPD5vfHIoG6IIWg5dc",
-#     "authDomain": "travel-b97bd.firebaseapp.com",
-#     "databaseURL": "https://travel-b97bd.firebaseio.com",
-#     "projectId": "travel-b97bd",
-#     "storageBucket": "travel-b97bd.appspot.com",
-#     "messagingSenderId": "850989183342",
-#     "appId": "1:850989183342:web:6eb717f390c612c8"
-# }
-#
-# app = Flask(__name__)
-# firebase = pyrebase.initialize_app(config)
-# db = firebase.database()
-# auth = firebase.auth()
-# app.config['SECRET_KEY'] = 'AIzaSyDrpj7_GwCxq2krshdqkainpdlFvLl9WNI'
-# app.config['GOOGLE_OAUTH2_CLIENT_ID'] = '850989183342-i3f30kop5phf368pbmaajas6ii9i3v1b.apps.googleusercontent.com'
-# app.config['GOOGLE_OAUTH2_CLIENT_SECRET'] = 'W4k_wh73Su4uTgRpyVqKUtL1'
-#
-# oauth2 = UserOAuth2(app)
-#
-# @app.route('/info')
-# @oauth2.required
-# def info():
-#     return "Hello, {} ({})".format(oauth2.email, oauth2.user_id)
-#
-#
-# @app.route('/optional')
-# def optional():
-#     if oauth2.has_credentials():
-#         return '로그인!'
-#     else:
-#         return '로그인 하세여!'
-#
-#
-# @app.route("/")
-# @app.route("/login", methods=['POST'])
-# def Login():
-#     unsuccessful = 'Please check your credentials'
-#     successful = 'Login successful'
-#     if request.method == 'POST':
-#         try:
-#             return render_template('Main.html', s=successful)
-#         except:
-#             return render_template('Login.html', us=unsuccessful)
-#     return render_template(
-#                 'Login.html',
-#                 title="Travel",
-#             )
-#
-# @app.route("/Main", methods=['GET', 'POST'])
-# @oauth2.required
-# def Main():
-#     id = request.form['id']
-#     return render_template('Main.html', id=id)
-
-import os
-import json
-import datetime
-
-from flask import Flask, url_for, redirect, \
-    render_template, session, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_required, login_user, \
-    logout_user, current_user, UserMixin
-from requests_oauthlib import OAuth2Session
-from requests.exceptions import HTTPError
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-os.environ['DEBUG'] = '1'
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-"""App Configuration"""
-
-
-class Auth:
-    """Google Project Credentials"""
-    CLIENT_ID = ('850989183342-i3f30kop5phf368pbmaajas6ii9i3v1b.apps.googleusercontent.com')
-    CLIENT_SECRET = 'W4k_wh73Su4uTgRpyVqKUtL1'
-    REDIRECT_URI = 'http://localhost:5000/oauth2authorize'
-    AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
-    TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
-    RESPONSE_URI = 'http://localhost:5000/oauth2callback'
-    SCOPE = ['profile', 'email']
-
-
-class Config:
-    """Base config"""
-    APP_NAME = "Test Google Login"
-    SECRET_KEY = "AIzaSyDrpj7_GwCxq2krshdqkainpdlFvLl9WNI"
-
-
-class DevConfig(Config):
-    """Dev config"""
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "test.db")
-
-
-class ProdConfig(Config):
-    """Production config"""
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "prod.db")
-
+import pyrebase
+from flask import *
 
 config = {
-    "dev": DevConfig,
-    "prod": ProdConfig,
-    "default": DevConfig
+    "apiKey": "AIzaSyDmn0dJl4lvTASIPtPD5vfHIoG6IIWg5dc",
+    "authDomain": "travel-b97bd.firebaseapp.com",
+    "databaseURL": "https://travel-b97bd.firebaseio.com",
+    "projectId": "travel-b97bd",
+    "storageBucket": "travel-b97bd.appspot.com",
+    "messagingSenderId": "850989183342",
+    "appId": "1:850989183342:web:6eb717f390c612c8"
 }
 
-"""APP creation and configuration"""
 app = Flask(__name__)
-app.config.from_object(config['dev'])
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
-login_manager.session_protection = "strong"
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+auth = firebase.auth()
+group_post = ""
 
-""" DB Models """
-
-
-class User(db.Model, UserMixin):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=True)
-    avatar = db.Column(db.String(200))
-    tokens = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-""" OAuth Session creation """
-
-
-def get_google_auth(state=None, token=None):
-    if token:
-        return OAuth2Session(Auth.CLIENT_ID, token=token)
-    if state:
-        return OAuth2Session(
-            Auth.CLIENT_ID,
-            state=state,
-            redirect_uri=Auth.REDIRECT_URI)
-    oauth = OAuth2Session(
-        Auth.CLIENT_ID,
-        redirect_uri=Auth.REDIRECT_URI,
-        scope=Auth.SCOPE)
-    return oauth
-
-
-@app.route('/')
-@login_required
-def index():
-    return render_template('main.html')
-
-
-@app.route('/login')
+@app.route("/")
+@app.route("/login", methods=['GET','POST'])
 def login():
-    logout_user()
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    google = get_google_auth()
-    auth_url, state = google.authorization_url(
-        Auth.AUTH_URI, access_type='offline')
-    session['oauth_state'] = state
-    return render_template('login.html', auth_url=auth_url)
+    return render_template('Login.html')
 
-
-@app.route('/oauth2authorize')
-def callback():
-    if current_user is not None and current_user.is_authenticated:
-        return redirect(url_for('index'))
-    if 'error' in request.args:
-        if request.args.get('error') == 'access_denied':
-            return 'You denied access.'
-        return 'Error encountered.'
-    if 'code' not in request.args and 'state' not in request.args:
-        return redirect(url_for('login'))
-    else:
-        google = get_google_auth(state=session['oauth_state'])
-        try:
-            token = google.fetch_token(Auth.TOKEN_URI, client_secret=Auth.CLIENT_SECRET,
-                                    code = "/oauth2callback",
-                                    authorization_response = Auth.REDIRECT_URI)
-        except HTTPError:
-            return 'HTTPError occurred.'
-        google = get_google_auth(token=token)
-        resp = google.get(Auth.USER_INFO)
-        if resp.status_code == 200:
-            user_data = resp.json()
-            email = user_data['email']
-            user = User.query.filter_by(email=email)
-            if user is None:
-                user = User()
-                user.email = email
-            user.name = user_data['name']
-            print(token)
-            user.tokens = json.dumps(token)
-            user.avatar = user_data['picture']
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('index'))
-        return 'Could not fetch your information.'
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/main')
-@login_required
+@app.route("/main", methods=['GET', 'POST'])
 def main():
-    return render_template('login.html', auth_url=auth_url)
+    return render_template('Main.html')
 
+@app.route("/getData", methods=['GET', 'POST'])
+def getData():
+    selected_group_id = request.form.get('selected_group_id', 0)
+    group_post = db.child("posts").child("selected_group_id").get()
+    return json.dumps('traveldiary')
 
-@app.after_request
-def set_response_headers(response):
-	response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-	response.headers['Pragma'] = 'no-cache'
-	response.headers['Expires'] = '0'
-	return response
+@app.route("/getDatas", methods=['GET', 'POST'])
+def getDatas():
+    group_post = db.child("posts").get()
+    return redirect(url_for("traveldiary"))
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return render_template('Login.html')
+
+@app.route("/traveldiary", methods=['GET', 'POST'])
+def traveldiary(posts=None):
+    posts = group_post
+    return render_template('TravelDiary.html', post=posts)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    return render_template('Upload.html')
+
+@app.route('/citylist', methods=['GET', 'POST'])
+def citylist():
+    city_list = db.child("citylist").get()
+    return json.dumps(city_list.val())
+
+@app.route("/post", methods=['GET', 'POST'])
+def post():
+    return render_template('Post.html')
+
+#
+# def traveldiary(id=None):
+#     city_name = request.form.get('selected_group_id', 0)
+#     return render_template('TravelDiary.html', city_name)
+#     return redirect(url_for('main'))
+    # city_post = db.child("diary").get()
+    # to = todo.val()
+	# return
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+  app.run(host="0.0.0.0",debug=True)
 # -> main.html
